3c57e1ef1ee2e2ea90f383089effd31e032b30ce
Ryan <ryan.bugge@tuta.io>
2021-11-12 00:18:14 -0500

    up to jwt on mern c notes

---
+++ frontend/src/components/Path.js
@@ -0,0 +1,9 @@
+const app_name = 'web-app-kf2me'
+exports.buildPath = function buildPath(route) {
+    if (process.env.NODE_ENV === 'production') {
+        return 'https://' + app_name + '.ondigitalocean.app/' + route;
+    }
+    else {
+        return 'http://localhost:5000/' + route;
+    }
+}
---
+++ server.js
@@ -0,0 +1,132 @@
+var cardList =
+    [
+        'Roy Campanella',
+        'Paul Molitor',
+        'Tony Gwynn',
+        'Dennis Eckersley',
+        'Reggie Jackson',
+        'Gaylord Perry',
+        'Buck Leonard',
+        'Rollie Fingers',
+        'Charlie Gehringer',
+        'Wade Boggs',
+        'Carl Hubbell',
+        'Dave Winfield',
+        'Jackie Robinson',
+        'Ken Griffey, Jr.',
+        'Al Simmons',
+        'Chuck Klein',
+        'Mel Ott',
+        'Mark McGwire',
+        'Nolan Ryan',
+        'Ralph Kiner',
+        'Yogi Berra',
+        'Goose Goslin',
+        'Greg Maddux',
+        'Frankie Frisch',
+        'Ernie Banks',
+        'Ozzie Smith',
+        'Hank Greenberg',
+        'Kirby Puckett',
+        'Bob Feller',
+        'Dizzy Dean',
+        'Joe Jackson',
+        'Sam Crawford',
+        'Barry Bonds',
+        'Duke Snider',
+        'George Sisler',
+        'Ed Walsh',
+        'Tom Seaver',
+        'Willie Stargell',
+        'Bob Gibson',
+        'Brooks Robinson',
+        'Steve Carlton',
+        'Joe Medwick',
+        'Nap Lajoie',
+        'Cal Ripken, Jr.',
+        'Mike Schmidt',
+        'Eddie Murray',
+        'Tris Speaker',
+        'Al Kaline',
+        'Sandy Koufax',
+        'Willie Keeler',
+        'Pete Rose',
+        'Robin Roberts',
+        'Eddie Collins',
+        'Lefty Gomez',
+        'Lefty Grove',
+        'Carl Yastrzemski',
+        'Frank Robinson',
+        'Juan Marichal',
+        'Warren Spahn',
+        'Pie Traynor',
+        'Roberto Clemente',
+        'Harmon Killebrew',
+        'Satchel Paige',
+        'Eddie Plank',
+        'Josh Gibson',
+        'Oscar Charleston',
+        'Mickey Mantle',
+        'Cool Papa Bell',
+        'Johnny Bench',
+        'Mickey Cochrane',
+        'Jimmie Foxx',
+        'Jim Palmer',
+        'Cy Young',
+        'Eddie Mathews',
+        'Honus Wagner',
+        'Paul Waner',
+        'Grover Alexander',
+        'Rod Carew',
+        'Joe DiMaggio',
+        'Joe Morgan',
+        'Stan Musial',
+        'Bill Terry',
+        'Rogers Hornsby',
+        'Lou Brock',
+        'Ted Williams',
+        'Bill Dickey',
+        'Christy Mathewson',
+        'Willie McCovey',
+        'Lou Gehrig',
+        'George Brett',
+        'Hank Aaron',
+        'Harry Heilmann',
+        'Walter Johnson',
+        'Roger Clemens',
+        'Ty Cobb',
+        'Whitey Ford',
+        'Willie Mays',
+        'Rickey Henderson',
+        'Babe Ruth'
+    ];
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.set('port', (process.env.PORT || 5000));
+app.use(cors());
+app.use(bodyParser.json());
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('frontend/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
+app.listen(PORT, () => {
+    console.log('Server listening on port ' + PORT);
+});
+
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://admin:fYC1BsmoZQI9SIUA@cluster0.mufuf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// const url = 'mongodb+srv://RickLeinecker:COP4331Rocks@cluster0-4pisv.mongodb.net/COP4331?retryWrites=true&w=majority'
+const client = new MongoClient(url);
+client.connect();
+
+var api = require('./api.js');
+api.setApp( app, client );
