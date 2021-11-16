291c7a5d6191b55135ef9fe172669ec7f45794fb
Ryan <ryan.bugge@tuta.io>
2021-11-15 20:07:03 -0500

    test backend

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.set('port', (process.env.PORT || 5000));
 app.use(cors());
 app.use(bodyParser.json());
 // Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
+// if (process.env.NODE_ENV === 'production') {
+//     // Set static folder
     app.use(express.static('frontend/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     });
-}
+// }
 app.listen(PORT, () => {
     console.log('Server listening on port ' + PORT);
 });
