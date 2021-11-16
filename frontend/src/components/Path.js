cbbe79354a3c1bd0b7ed7826de8d37ed8ad63176
Ryan <ryan.bugge@tuta.io>
2021-11-15 20:50:48 -0500

    path test

--- frontend/src/components/Path.js
+++ frontend/src/components/Path.js
@@ -1,9 +1,11 @@
-const app_name = 'web-app-kf2me'
+// const app_name = 'web-app-kf2me'
 exports.buildPath = function buildPath(route) {
-    if (process.env.NODE_ENV === 'production') {
-        return 'https://' + app_name + '.ondigitalocean.app/' + route;
-    }
-    else {
-        return 'http://localhost:5000/' + route;
-    }
+    // if (process.env.NODE_ENV === 'production') {
+    //     return 'https://' + app_name + '.ondigitalocean.app/' + route;
+    // }
+    // else {
+    //     return 'http://localhost:5000/' + route;
+    // }
+
+    return 'http://137.184.153.148/' + route;
 }
