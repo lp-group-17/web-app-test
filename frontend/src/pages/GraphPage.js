233d120bae5b7f7f0977fba1a17ffd4da00f1b49
Lostmaniac9 <jdog2349@gmail.com>
2021-11-22 12:13:08 -0500

    added a dot because it was needed

--- frontend/src/pages/GraphPage.js
+++ frontend/src/pages/GraphPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import BarGraph from './components/BarGraph';
+import BarGraph from '../components/BarGraph';
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
 import Form from 'react-bootstrap/Form';
