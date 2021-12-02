9b64001845a06a5be9ef6a1311703e0dc012f7e0
rbugge <ryan.bugge@tuta.io>
2021-12-01 21:09:09 -0500

    test

--- server.js
+++ server.js
@@ -1,32 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
+// Top two lines added to decode line to connect to db
+global.TextEncoder = require("util").TextEncoder;
+global.TextDecoder = require("util").TextDecoder;
+
+const express = require ('express');
 const app = express();
-app.set('port', (process.env.PORT || 5000));
-app.use(cors());
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+// Middlewares
 app.use(bodyParser.json());
-// Server static assets if in production
-// if (process.env.NODE_ENV === 'production') {
-//     // Set static folder
-    app.use(express.static('frontend/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    });
-// }
-app.listen(PORT, () => {
-    console.log('Server listening on port ' + PORT);
+
+// use middleware to check for auth
+
+// Import routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
+
+app.use(express.static('frontend/build'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 });
 
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb+srv://admin:fYC1BsmoZQI9SIUA@cluster0.mufuf.mongodb.net/MoodTracker?retryWrites=true&w=majority';
-// const url = 'mongodb+srv://RickLeinecker:COP4331Rocks@cluster0-4pisv.mongodb.net/COP4331?retryWrites=true&w=majority'
-const client = new MongoClient(url);
-client.connect();
+// Routes
+app.get('/', (req, res) => {
+    res.send('on home');
+});
 
-var api = require('./api.js');
-api.setApp( app, client );
+// Connect to db
+mongoose.connect(process.env.DB_CONNECTION,
+{useNewUrlParser: true},
+() => console.log('connected to db')
+);
 
-// var firebase = require('firebase');
-// var firebaseui = require('firebaseui');
+// Listen to the server
+app.listen(5000);
